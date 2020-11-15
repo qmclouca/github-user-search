3f0c137c9056ebad37c96c08eae48d57c54afd13
qmclouca <qmclouca@gmail.com>
2020-11-14 22:10:46 -0300

    Home design adjusts

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,19 +4,23 @@ import './styles.scss';
 import ButtonIcon from './../../core/assets/components/ButtonIcon/index';
 
 const Home = () => (
-    <div className = "home-container">
-        <div className = "row home-content card-base border-radius-20">
+    <div>
+        <div className = "row">
             <div className = "col-6 home-text">
                 <h1 className = "text-title"> 
-                    Desafio Capítulo 3, <br/>
+                    Desafio do Capítulo 3, <br/>
                     Bootcamp DevSuperior                
                 </h1>
                 <p className = "text-subtitle">
                 Bem-vindos ao desafio do capítulo 3 do Bootcamp DevSuperior. <br/>
+                <br/>
                 Favor observar as instruções passadas no capítulo sobre a elaboração <br/>
                 deste projeto. <br/>
+                <br/>
                 Este design foi adaptado a partir de Ant Design System for Figma, de <br/>
-                Mateusz Wierzbicki: antforfigma@gmail.com
+                Mateusz Wierzbicki: <a href="antforfigma@gmail.com">antforfigma@gmail.com</a>
+                <br/>
+                <br/>
                 </p>
                 <Link to = "/search">
                     <ButtonIcon text="Começar"/>
