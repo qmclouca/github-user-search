8564bba0d5ab1405ed9bbb5b12164d90b4b634b9
qmclouca <qmclouca@gmail.com>
2020-11-17 21:42:36 -0300

    Recovering data from GITHUB API

--- src/core/assets/components/utils/request.ts
+++ src/core/assets/components/utils/request.ts
@@ -7,7 +7,7 @@ type RequestParams = {
     params?: object;
 }
 
-const BASE_URL = 'http://api.github/users/';
+const BASE_URL = 'http://api.github/users';
 
 export const makeRequest = ({method = 'GET', url, data, params}: RequestParams) => {
     return axios({
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -2,34 +2,38 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import Form from '../../core/assets/components/Form';
 import ResultPhotoLoader from '../../core/assets/components/Loaders/ResultPhotoLoader';
-import Results from '../../core/assets/components/Results';
 import { makeRequest } from '../../core/assets/components/utils/request';
 import { person }  from '../../core/assets/components/utils/types'
 import './styles.scss';
 
 type ParamsType = {
-    name: string;
-}
+    login: string;
+    }
+    
 
 const Search = () => {
-    const {name} = useParams<ParamsType>();
+    const {login} = useParams<ParamsType>();
     const [isLoading, setIsLoading] = useState(false);
     const [person, setPerson] = useState<person>();
     
     useEffect(() => {
         setIsLoading(true);
-        makeRequest({url:`/${name}`})
+        makeRequest({url:`/${login}`})
             .then(response => setPerson(response.data))
             .finally(() => setIsLoading(false));
-    }, [name]);
+    }, [login]);
 
 return (
     <div> 
         {isLoading ? <ResultPhotoLoader /> : (
+            <>
             <div className="search-form-base">
                 <h1><Form /></h1>
-                <h1><Results /></h1>
             </div>
+            <div>
+                {person?.name}
+            </div>
+            </>
         )}
     </div>
 )
